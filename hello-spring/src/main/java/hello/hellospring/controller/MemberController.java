b07bd10e84725b59ed8bc36c3edb98a8feee8145
Taehyun Kim <k1m743hyun@icloud.com>
2022-09-28 18:45:40 +0900

    Learn Section 5. 회원 관리 예제 - 웹 MVC 개발

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -22,5 +25,23 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
         //memberService.setMemberRepository(); // 누구나 호출 가능
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member = " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
