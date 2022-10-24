d99f0a03bf9ffec719137e3b6b59866cf596d31b
Taehyun Kim <k1m743hyun@icloud.com>
2022-10-24 15:10:31 +0900

    Learn Section 6-3. 스프링 통합 테스트

--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -17,29 +17,15 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @Transactional
 class MemberServiceIntegrationTest {
 
-//    MemberService memberService;
-//    MemoryMemberRepository memberRepository;
-
-//    @BeforeEach
-//    public void beforeEach() {
-//        memberRepository = new MemoryMemberRepository();
-//        memberService = new MemberService(memberRepository);
-//    }
-
-//    @AfterEach
-//    public void afterEach() {
-//        memberRepository.clearStore();
-//    }
-
     @Autowired MemberService memberService;
     @Autowired MemberRepository memberRepository;
 
     @Test
-    //void join() {
     void 회원가입() {
+
         // given
         Member member = new Member();
-        member.setName("hello");
+        member.setName("spring");
 
         // when
         Long saveId = memberService.join(member);
@@ -51,6 +37,7 @@ class MemberServiceIntegrationTest {
 
     @Test
     public void 중복_회원_예외() {
+
         // given
         Member member1 = new Member();
         member1.setName("spring");
@@ -63,22 +50,6 @@ class MemberServiceIntegrationTest {
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 
-//        try {
-//            memberService.join(member2);
-//            fail();
-//        } catch (IllegalStateException e) {
-//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-//        }
-
         // then
     }
-
-//    @Test
-//    void findMembers() {
-//    }
-//
-//    @Test
-//    void findOne() {
-//    }
-
 }
