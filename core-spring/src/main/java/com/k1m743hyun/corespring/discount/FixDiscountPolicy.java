7d5aa94a65378b5608c6dac01c9f0f764838cb9b
Taehyun Kim <k1m743hyun@icloud.com>
2023-02-27 15:50:08 +0900

    Update core-spring/discount

--- core-spring/src/main/java/com/k1m743hyun/corespring/discount/FixDiscountPolicy.java
+++ core-spring/src/main/java/com/k1m743hyun/corespring/discount/FixDiscountPolicy.java
@@ -5,7 +5,7 @@ import com.k1m743hyun.corespring.member.Member;
 
 public class FixDiscountPolicy implements DiscountPolicy {
 
-    private int discountFixAmount = 1000; // 1000원 할인
+    private final int discountFixAmount = 1000; // 1000원 할인
 
     @Override
     public int discount(Member member, int price) {
