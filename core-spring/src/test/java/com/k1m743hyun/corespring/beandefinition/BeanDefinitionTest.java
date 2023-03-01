6950e3b3d10dc25dbabe65c53c252bad099a002f
Taehyun Kim <k1m743hyun@icloud.com>
2023-03-01 21:22:15 +0900

    Learn Section 4-8. 스프링 빈 설정 메타 정보 - BeanDefinition

---
+++ core-spring/src/test/java/com/k1m743hyun/corespring/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,29 @@
+package com.k1m743hyun.corespring.beandefinition;
+
+import com.k1m743hyun.corespring.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class BeanDefinitionTest {
+
+     // AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+    GenericXmlApplicationContext applicationContext = new GenericXmlApplicationContext("appConfig.xml");
+
+     @Test
+     @DisplayName("빈 메타 정보 확인")
+    void findApplicationBean() {
+
+         String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+         for (String beanDefinitionName : beanDefinitionNames) {
+             BeanDefinition beanDefinition = applicationContext.getBeanDefinition(beanDefinitionName);
+
+             if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                 System.out.println("beanDefinitionName = " + beanDefinitionName);
+                 System.out.println("beanDefinition = " + beanDefinition);
+             }
+         }
+     }
+}
