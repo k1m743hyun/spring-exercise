ac81fc8bf84e544e7bbc43de0e1fcc35bcb1541f
Taehyun Kim <k1m743hyun@icloud.com>
2023-03-06 22:02:34 +0900

    Learn Section 5-4. 싱글톤 방식의 주의점

---
+++ core-spring/src/test/java/com/k1m743hyun/corespring/singleton/StatefulService.java
@@ -0,0 +1,16 @@
+package com.k1m743hyun.corespring.singleton;
+
+public class StatefulService {
+
+    private int price; // 상태를 유지하는 필드 10000 => 20000
+
+    public void order(String name, int price) {
+        System.out.println("name = " + name);
+        System.out.println("price = " + price);
+        this.price = price; // 문제가 되는 코드
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
---
+++ core-spring/src/test/java/com/k1m743hyun/corespring/singleton/StatefulServiceTest.java
@@ -0,0 +1,38 @@
+package com.k1m743hyun.corespring.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(TestConfig.class);
+
+        StatefulService statefulService1 = applicationContext.getBean(StatefulService.class);
+        StatefulService statefulService2 = applicationContext.getBean(StatefulService.class);
+
+        // Thread A: A 사용자가 10000원 주문
+        statefulService1.order("userA", 10000);
+        // Thread B: B 사용자가 20000원 주문
+        statefulService2.order("userB", 20000);
+
+        // Thread A: A 사용자가 주문 금액을 조회
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);
+
+        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
