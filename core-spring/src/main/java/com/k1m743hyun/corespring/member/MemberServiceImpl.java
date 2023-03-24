286399d4b9666917acffb14c8faccf161030b2ac
Taehyun Kim <k1m743hyun@icloud.com>
2023-03-24 22:33:26 +0900

    Learn Section 6-1. 컴포넌트 스캔과 의존 관계 자동 주입 시작하기

--- core-spring/src/main/java/com/k1m743hyun/corespring/member/MemberServiceImpl.java
+++ core-spring/src/main/java/com/k1m743hyun/corespring/member/MemberServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Component("memberService2")
 public class MemberServiceImpl implements MemberService {
 
     // private final MemberRepository memberRepository = new MemoryMemberRepository();
