7f3c3ddd8dbbb7dcd9cb2d4f2eeb9247c094c5b1
Taehyun Kim <k1m743hyun@icloud.com>
2023-03-27 14:44:12 +0900

    Learn Section 6-2. 탐색 위치와 기본 스캔 대상

--- core-spring/src/main/java/com/k1m743hyun/corespring/AutoAppConfig.java
+++ core-spring/src/main/java/com/k1m743hyun/corespring/AutoAppConfig.java
@@ -4,9 +4,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
-@Configuration
 @ComponentScan(
+        // basePackages = "com.k1m743hyun.corespring.member",
+        // basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
+@Configuration
 public class AutoAppConfig {
 }
