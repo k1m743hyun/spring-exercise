b06ca876034d4ff578a0955a1569d548c16d114a
Taehyun Kim <k1m743hyun@icloud.com>
2023-03-29 18:16:25 +0900

    Learn Section 7-4. Lombok과 최신 트렌드

--- core-spring/src/main/java/com/k1m743hyun/corespring/order/OrderServiceImpl.java
+++ core-spring/src/main/java/com/k1m743hyun/corespring/order/OrderServiceImpl.java
@@ -3,20 +3,23 @@ package com.k1m743hyun.corespring.order;
 import com.k1m743hyun.corespring.discount.DiscountPolicy;
 import com.k1m743hyun.corespring.member.Member;
 import com.k1m743hyun.corespring.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+//    private final Object object;
 
     //@Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
