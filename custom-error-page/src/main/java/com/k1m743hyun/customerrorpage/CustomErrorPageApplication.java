4c4fd1e6554b59e069c1dbe1e35f96672eab81d4
Taehyun Kim <k1m743hyun@icloud.com>
2023-04-04 18:45:52 +0900

    Create custom-error-page/

---
+++ custom-error-page/src/main/java/com/k1m743hyun/customerrorpage/CustomErrorPageApplication.java
@@ -0,0 +1,13 @@
+package com.k1m743hyun.customerrorpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomErrorPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomErrorPageApplication.class, args);
+	}
+
+}
---
+++ custom-error-page/src/main/java/com/k1m743hyun/customerrorpage/controller/CustomErrorController.java
@@ -0,0 +1,8 @@
+package com.k1m743hyun.customerrorpage.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CustomErrorController {
+
+}
