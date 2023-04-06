39fc3f1b24fe6b04022b9683ea2cb5c772a5596e
Taehyun Kim <k1m743hyun@icloud.com>
2023-04-06 18:22:26 +0900

    Create spring-transaction-propagation/

---
+++ spring-transaction-propagation/src/main/java/com/k1m743hyun/springtransactionpropagation/SpringTransactionPropagationApplication.java
@@ -0,0 +1,13 @@
+package com.k1m743hyun.springtransactionpropagation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransactionPropagationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTransactionPropagationApplication.class, args);
+	}
+
+}
